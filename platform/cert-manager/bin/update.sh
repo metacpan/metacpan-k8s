f2a977426ecaff7bc157801e25670a4276cbf1bc
Shawn Sorichetti <shawn@coloredblocks.com>
2022-10-14 12:53:12 -0400

    Upgrade cert-manager to 1.9.1
    Update to the latest release of cert-manager in hopes of fixing the bug
    that plagues the renewal of certificates for bare domains with
    CloudFlare.

--- platform/cert-manager/bin/update.sh
+++ platform/cert-manager/bin/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="v1.7.1"
+VERSION="v1.9.1"
 URL="https://github.com/jetstack/cert-manager/releases/download/$VERSION/cert-manager.yaml"
 
 wget -O base/deploy.yaml $URL
